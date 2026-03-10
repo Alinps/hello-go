0eaea4b349fe2df9d2dba3b1b3c24fcca6b9a7f4
alin <alinps8406@gmail.com>
2026-03-10 23:26:25 +0530

    prompt for user name

--- main.go
+++ main.go
@@ -3,23 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var day = "Monday"
-
-	if day == "Monday"{
-		fmt.Println("Today is Monday")
-	}else if day == "Tuesday"{
-		fmt.Println("Today is Tuesday")
-	}else if day == "Wednesday"{
-		fmt.Println("Today is Wednesday")
-	}else if day == "Thursday"{
-		fmt.Println("Today is Thursday")
-	}else if day == "Friday"{
-		fmt.Println("Today is Friday")
-	}else if day == "Saturday"{
-		fmt.Println("Today is Saturday")
-	}else if day == "Sunday"{
-		fmt.Println("Today is Sunday")
-	}else {
-		fmt.Println("Invalid day")
-	}
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Println("Hello", name)
 }
