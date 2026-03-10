54d6b0f3afe37b001983e234935f3445a0f58b8d
alin <alinps8406@gmail.com>
2026-03-10 23:57:14 +0530

    reading multiple words including spaces

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	var name string
-	var age int
-	var place string
-	fmt.Print("Enter name, age and place: ")
-	fmt.Scan(&name, &age,&place)
+	fmt.Print("Enter your name: ")
+	name, _ = reader.ReadString('\n')
 	fmt.Println("Name: ", name)
-	fmt.Println("Age: ", age)
-	fmt.Println("Place",place)
 }
