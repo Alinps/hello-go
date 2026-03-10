18dfeb4c5eabddf598ba67de7714235c60115bff
alin <alinps8406@gmail.com>
2026-03-10 23:10:18 +0530

    update main.go to check voting eligibility based on age

--- main.go
+++ main.go
@@ -3,22 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	var name = "Alin"
-	var age int = 24
-	place := "trivandrum"
-	const freedom int = 1947
-	const country = "India"
-	var isOnline bool = true
-	var year int = 2026
-	var college string = "college of engineering perumon"
-	fmt.Println(name)
-	fmt.Println(age)
-	fmt.Println(place)
-	fmt.Println(country)
-	fmt.Println(freedom)
-	fmt.Println(isOnline)
-	fmt.Println(year)
-	fmt.Println(college)
-	
+	var age int = 16
+	if age >= 18 {
+		fmt.Println("You are eligible to vote")
+	} else {
+		fmt.Println("You are not eligible to vote")
+	}
+
 }
