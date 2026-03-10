71256482429f55534883a3715045df6dd2089b71
alin <alinps8406@gmail.com>
2026-03-10 23:59:37 +0530

    simple program on reading data from terninal

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	var name string
+	var age int
 	fmt.Print("Enter your name: ")
 	name, _ = reader.ReadString('\n')
+	fmt.Print("Enter your age")
+	fmt.Scan(&age)
 	fmt.Println("Name: ", name)
+	fmt.Println("Age: ",age)
 }
