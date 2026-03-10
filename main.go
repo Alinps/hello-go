7f3b30c298c48f63ca48fa24de7d50f1f4a9af59
alin <alinps8406@gmail.com>
2026-03-10 23:07:21 +0530

    initialize go.mod and expand main.go with additional variables and print statements

--- main.go
+++ main.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	var name = "Alin"
+	var age int = 24
+	place := "trivandrum"
+	const freedom int = 1947
+	const country = "India"
+	var isOnline bool = true
+	var year int = 2026
+	var college string = "college of engineering perumon"
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(place)
+	fmt.Println(country)
+	fmt.Println(freedom)
+	fmt.Println(isOnline)
+	fmt.Println(year)
+	fmt.Println(college)
+	
 }
