a5508f032eb79a4b63c9d45078c2f05a596c3819
alin <alinps8406@gmail.com>
2026-03-11 23:27:59 +0530

    functions

--- main.go
+++ main.go
@@ -2,20 +2,10 @@ package main
 
 import "fmt"
 
-func main() {
-	var num int
-	var fact int = 1
-	fmt.Print("Enter the number: ")
-	fmt.Scan(&num)
-	if num == 0 {
-		fmt.Println("O has no factorial")
-	} else if num == 1 {
-		fmt.Println("factorial of 1 is 1")
-	} else {
-		for i := num; i >= 1; i-- {
-			fact = fact * i
-		}
-		fmt.Println("Factorial of ", num, " is ", fact)
-	}
+func greet(name string) {
+	fmt.Println("Hello ", name)
+}
 
+func main() {
+	greet("Alin")
 }
