ee1be525434a08dc5585d68e6a106f0bd52de1e5
alin <alinps8406@gmail.com>
2026-03-11 23:49:19 +0530

    function with return

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import "fmt"
 func greet(name string) {
 	fmt.Println("Hello ", name)
 }
+func add(a int, b int) int {
+	var sum int = a + b
+	return sum
+}
 
 func main() {
 	greet("Alin")
+	var result int = add(5, 4)
+	fmt.Println("Result= ", result)
 }
