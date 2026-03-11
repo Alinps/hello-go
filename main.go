6d123d31384af65bda9d67144ecdbb43ccafa956
alin <alinps8406@gmail.com>
2026-03-12 00:04:05 +0530

    name return in functions

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ func divide(a int, b int) (int, int) {
 	var remainder int = a % b
 	return quotient, remainder
 }
+func add(a int, b int) (sum int) {
+	sum = a + b
+	return
+}
+
 func main() {
 	var q int
 	var r int
+	var sumresult int
 	q, r = divide(10, 3)
+	sumresult = add(4, 3)
 	fmt.Println("Quotien: ", q)
 	fmt.Println("Remainder: ", r)
+	fmt.Println("Sum: ", sumresult)
 }
