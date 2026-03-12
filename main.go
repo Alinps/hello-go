a8fdfef183d2645d04e668af7a7f43e8c698128f
alin <alinps8406@gmail.com>
2026-03-12 22:28:32 +0530

    string operations

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"strings"
 )
 
-func divide(a int, b int) (int, error) {
-	if b == 0 {
-		return 0, errors.New("Cannot divide by zero")
-	}
-	return a / b, nil
-}
 func main() {
-	var a int
-	var b int
-	fmt.Print("Enter the first number: ")
-	fmt.Scan(&a)
-	fmt.Print("Enter the second number: ")
-	fmt.Scan(&b)
-	result, err := divide(a, b)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-	fmt.Println("Result ", result)
+	var name string = "Alin p s"
+	var firstname string = "Aneesha"
+	var lastname string = "Alin"
+	var text string = "      hello world    "
+	var language string = "I love, python"
+	fmt.Println("length:", len(name))
+	fmt.Println(firstname + " " + lastname)
+	fmt.Println(firstname == lastname)
+	fmt.Println(firstname != lastname)
+	fmt.Println(strings.ToUpper(name))
+	fmt.Println(strings.ToLower(name))
+	fmt.Println("before triming whitespace: ", text)
+	fmt.Println("After triming whitespace: ", strings.TrimSpace(text))
+	fmt.Println(strings.Replace(language, "python", "golang", 1))
+	fmt.Println(strings.Split(language, ""))
+
 }
