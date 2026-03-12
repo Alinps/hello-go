112145ec70ec0e724a1418faafd2f5507e7a7c1f
alin <alinps8406@gmail.com>
2026-03-12 23:10:37 +0530

    array

--- main.go
+++ main.go
@@ -2,24 +2,42 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var name string = "Alin p s"
-	var firstname string = "Aneesha"
-	var lastname string = "Alin"
-	var text string = "      hello world    "
-	var language string = "I love, python"
-	fmt.Println("length:", len(name))
-	fmt.Println(firstname + " " + lastname)
-	fmt.Println(firstname == lastname)
-	fmt.Println(firstname != lastname)
-	fmt.Println(strings.ToUpper(name))
-	fmt.Println(strings.ToLower(name))
-	fmt.Println("before triming whitespace: ", text)
-	fmt.Println("After triming whitespace: ", strings.TrimSpace(text))
-	fmt.Println(strings.Replace(language, "python", "golang", 1))
-	fmt.Println(strings.Split(language, ""))
+	var numbers [3]int
+	var name [4]string
+	var food [3]string
 
+	var cars [3]string = [3]string{"Mustang", "Ferrari", "lamborgini"}
+	var fruits [5]string = [5]string{"apple", "orange", "lemon", "watermelon"}
+	statuscode := [3]int{200, 300, 404}
+
+	name[0] = "Alin"
+	name[1] = "Aneesha"
+	name[2] = "Aleena"
+	name[3] = "Avanthika"
+
+	numbers[0] = 1
+	numbers[1] = 2
+	numbers[2] = 3
+
+	food[0] = "mandhi"
+	food[1] = "biriyani"
+	food[2] = "shawarma"
+
+	fmt.Println(numbers)
+	fmt.Println(name)
+	fmt.Println(food)
+	fmt.Println(cars)
+	fmt.Println(fruits)
+	for i := 0; i < len(fruits); i++ {
+		fmt.Println(fruits[i])
+	}
+	for i := 0; i < len(cars); i++ {
+		fmt.Printf("car %d: %s\n", i, cars[i])
+	}
+	for i := 0; i < len(statuscode); i++ {
+		fmt.Printf("Statuscode %d: %d\n", i, statuscode[i])
+	}
 }
