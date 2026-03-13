7cc36e3a0004e721158bf832447770d073942175
alin <alinps8406@gmail.com>
2026-03-13 23:31:35 +0530

    headers using map

--- main.go
+++ main.go
@@ -12,28 +12,12 @@ func main() {
 		"Aleena":    15,
 		"Avanthika": 13,
 	}
+	headers := map[string]string{
+		"Content-type":  "application/json",
+		"Accept":        "application/json",
+		"Authorization": "Bearer token",
+	}
 	fmt.Println(ages)
-	fmt.Println("Age of Aneesha: ", ages["Aneesha"])
-	fmt.Println("Age of Alin: ", ages["Alin"])
-	ages["Aleena"] = 16
-	ages["Roshni"] = 23
-	fmt.Println("Updated map: ", ages)
-
-	languages := map[string]string{
-		"Python":     "Django",
-		"Java":       "Spring boot",
-		"GoLang":     "Gin",
-		"JavaScript": "Express.js",
-	}
-	languages["C#"] = "ASP.net"
-
-	value, exists := ages["Alinps"]
-	fmt.Println("Value: ", value)
-	fmt.Println("Exists", exists)
-	fmt.Println(len(ages))
-	fmt.Println("Iterating map")
-	for key, value := range ages {
-		fmt.Println(key, value)
-	}
+	fmt.Println(headers)
 
 }
