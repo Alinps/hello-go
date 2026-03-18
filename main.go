4397bd18e60136776ad9501c1b46acac1fceb8e8
alin <alinps8406@gmail.com>
2026-03-18 22:41:42 +0530

    json decoding problem

--- main.go
+++ main.go
@@ -11,13 +11,24 @@ type User struct {
 	isActive bool
 	Weight   float64
 }
+type Obj struct {
+	Color string
+	Shape string
+}
 
 func main() {
 	jsonData := `{"Name":"Alin","Age":25,"isActive":true,"Weight":64.5}`
+	jsonData2 := `{"Color":"Black","Shape":"Circle"}`
 	var user User
+	var obj Obj
 	err := json.Unmarshal([]byte(jsonData), &user)
+	err2 := json.Unmarshal([]byte(jsonData2), &obj)
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
+	if err2 != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(user.Name)
+	fmt.Println(obj.Shape)
 }
