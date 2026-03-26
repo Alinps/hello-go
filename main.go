07ecd742fb1a0a83582df20094c5e6f2e68fbffb
alin <alinps8406@gmail.com>
2026-03-26 21:10:33 +0530

    interface

--- main.go
+++ main.go
@@ -1,34 +1,23 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
 
-type User struct {
-	Name     string
-	Age      int
-	isActive bool
-	Weight   float64
+type Speaker interface {
+	speak()
 }
-type Obj struct {
-	Color string
-	Shape string
+
+type Dog struct {
+	Name string
+}
+
+func (d Dog) speak() {
+	fmt.Println("Barks", d.Name)
 }
 
 func main() {
-	jsonData := `{"Name":"Alin","Age":25,"isActive":true,"Weight":64.5}`
-	jsonData2 := `{"Color":"Black","Shape":"Circle"}`
-	var user User
-	var obj Obj
-	err := json.Unmarshal([]byte(jsonData), &user)
-	err2 := json.Unmarshal([]byte(jsonData2), &obj)
-	if err != nil {
-		fmt.Println("Error: ", err)
+	var s Speaker
+	s = Dog{
+		Name: "Bruno",
 	}
-	if err2 != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(user.Name)
-	fmt.Println(obj.Shape)
+	s.speak()
 }
