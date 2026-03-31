98988787edadd86174571791fd7083c679907249
alin <alinps8406@gmail.com>
2026-03-31 10:02:53 +0530

    address opeerator

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-func task(wg *sync.WaitGroup) {
-	defer wg.Done() // mark task complete
-	fmt.Println("Task done")
-}
+import "fmt"
 
 func main() {
-	var wg sync.WaitGroup
-
-	wg.Add(1)        // number of goroutines
-	go task(&wg)     // run goroutine
-
-	wg.Wait()        // wait until done
+	x := 10
+	fmt.Println(&x)
 }
